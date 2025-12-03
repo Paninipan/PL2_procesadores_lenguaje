a703d478c1bd795776a13605b3efa93714b3f888
AlvaroPani <alvaro.paniaguac@edu.uah.es>
2025-12-03 09:44:19 +0100

    Cosas++

---
+++ PL3/Parte1/CSV_JSON_ParserBaseListener.java
@@ -0,0 +1,75 @@
+// Generated from C:/Users/alvar/OneDrive/Documentos/uni/3.1Cuatri/Procesadores/Laboratorio/PL2/PL3/Parte1/CSV_JSON_Parser.g4 by ANTLR 4.13.2
+
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ErrorNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
+
+/**
+ * This class provides an empty implementation of {@link CSV_JSON_ParserListener},
+ * which can be extended to create a listener which only needs to handle a subset
+ * of the available methods.
+ */
+@SuppressWarnings("CheckReturnValue")
+public class CSV_JSON_ParserBaseListener implements CSV_JSON_ParserListener {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterArchivo(CSV_JSON_Parser.ArchivoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitArchivo(CSV_JSON_Parser.ArchivoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterFila(CSV_JSON_Parser.FilaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitFila(CSV_JSON_Parser.FilaContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterCampo(CSV_JSON_Parser.CampoContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitCampo(CSV_JSON_Parser.CampoContext ctx) { }
+
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void enterEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void exitEveryRule(ParserRuleContext ctx) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitTerminal(TerminalNode node) { }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation does nothing.</p>
+	 */
+	@Override public void visitErrorNode(ErrorNode node) { }
+}
---
+++ PL3/Parte1/CSV_JSON_ParserListener.java
@@ -0,0 +1,39 @@
+// Generated from C:/Users/alvar/OneDrive/Documentos/uni/3.1Cuatri/Procesadores/Laboratorio/PL2/PL3/Parte1/CSV_JSON_Parser.g4 by ANTLR 4.13.2
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link CSV_JSON_Parser}.
+ */
+public interface CSV_JSON_ParserListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link CSV_JSON_Parser#archivo}.
+	 * @param ctx the parse tree
+	 */
+	void enterArchivo(CSV_JSON_Parser.ArchivoContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link CSV_JSON_Parser#archivo}.
+	 * @param ctx the parse tree
+	 */
+	void exitArchivo(CSV_JSON_Parser.ArchivoContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link CSV_JSON_Parser#fila}.
+	 * @param ctx the parse tree
+	 */
+	void enterFila(CSV_JSON_Parser.FilaContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link CSV_JSON_Parser#fila}.
+	 * @param ctx the parse tree
+	 */
+	void exitFila(CSV_JSON_Parser.FilaContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link CSV_JSON_Parser#campo}.
+	 * @param ctx the parse tree
+	 */
+	void enterCampo(CSV_JSON_Parser.CampoContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link CSV_JSON_Parser#campo}.
+	 * @param ctx the parse tree
+	 */
+	void exitCampo(CSV_JSON_Parser.CampoContext ctx);
+}
